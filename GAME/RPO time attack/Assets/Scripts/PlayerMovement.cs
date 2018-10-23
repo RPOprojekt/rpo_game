a3181c0155e847c531d5974a0f4bcd82737b2dca
Saso <saso.ivic@student.um.si>
2018-10-23 18:16:40 +0200

    2.commit

---
+++ GAME/RPO time attack/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public VirtualJoystick joystick;
+    public float speed = 150.0f;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rigid.velocity = joystick.InputDirection * speed;
+
+        //rigid.transform.rotation = Quaternion.LookRotation(joystick.InputDirection, Vector3.back);
+    }
+}
