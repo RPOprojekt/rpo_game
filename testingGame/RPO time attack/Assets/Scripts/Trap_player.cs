e97ddda82b5b9e68199dd6bec2f772b949588528
Saso <saso.ivic@student.um.si>
2018-10-24 19:39:10 +0200

    dodatno

---
+++ testingGame/RPO time attack/Assets/Scripts/Trap_player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_player : MonoBehaviour {
+
+    bool killed = false;
+
+    Vector2 startPosition = Vector2.zero;
+
+    private void Start()
+    {
+        startPosition = GameObject.FindGameObjectWithTag("Player").transform.position; //dobi zacetno pozicijo Playerja
+        //Debug.Log(startPosition);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) //ce se sprozi trigger
+    {
+        if (other.CompareTag("KillTrap"))
+        {
+            Debug.Log("You got killed");
+            killed = true;
+
+            transform.position = startPosition; //playerja vrze na zacetno pozicijo
+        }
+
+        if (other.CompareTag("SlowTrap"))
+        {
+            Debug.Log("You are slowed down for 5s");
+        }
+    }
+}
---
+++ testingGame/RPO time attack/Assets/Scripts/VrtenjeTopa.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrtenjeTopa : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        //object controller = GameObject.FindGameObjectsWithTag("GameController");
+
+        Rect bounds = new Rect(0, 0, Screen.width/3, Screen.height/3);
+
+        if (Input.GetMouseButtonDown(0) && ! bounds.Contains(Input.mousePosition))
+        {
+            var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+        }
+
+	}
+}
