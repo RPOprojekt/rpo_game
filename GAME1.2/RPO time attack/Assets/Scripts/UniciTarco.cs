cbd1e22b3259a070146387d75b83a56887728288
Saso <saso.ivic@student.um.si>
2018-10-28 13:12:54 +0100

    Game1.2; shooting, shooting animation, destroy targets, bullet collusion

---
+++ GAME1.2/RPO time attack/Assets/Scripts/UniciTarco.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniciTarco : MonoBehaviour {
+
+    public int stTarc; // ST TARC, KLJUCEV... SI NAREDI V POSEBEJ SKRIPTI (GameManeger)
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other) //ce se sprozi trigger metka
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(gameObject); //unici tarco
+        }
+    }
+}
