6896d16cdc90dc04eb5e5808f98c5babdae47a8f
blazv <blazvidovic420@gmail.com>
2018-11-03 14:41:27 +0100

    Game1.3; dodan arrow prefab in bowtrap

---
+++ GAME1.3/RPO time attack/Assets/Scripts/CameraFolow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () { //Late update run after all proces has been updated
+
+        transform.position = player.transform.position + offset; //vsaki frame se kamera premika za playerjem
+	}
+}
