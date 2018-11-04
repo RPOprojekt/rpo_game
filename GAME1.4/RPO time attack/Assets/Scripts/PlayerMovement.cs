4a0b71542782594547f49b7c8674ed373b9d7240
Saso <saso.ivic@student.um.si>
2018-11-04 19:07:41 +0100

    coinPickup, chestOpen, trapAnimation, targetHealth

---
+++ GAME1.4/RPO time attack/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public VirtualJoystick joystick;
+
+    public float speed = 5.0f;
+    private Rigidbody2D rigid;
+    private Quaternion targetRotation;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+      //  trap = new Trap_player();
+    }
+
+    private void Update()
+    {
+        var direction = joystick.InputDirection;
+        var angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+        
+        rigid.velocity = joystick.InputDirection * speed;
+
+        if (joystick.InputDirection != Vector2.zero) //če hodimo se vklopi animacija
+        {
+            animator.SetFloat("Speed", 1);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
+
+        //rigid.transform.rotation = Quaternion.LookRotation(joystick.InputDirection, Vector3.back);
+    }
+}
---
+++ GAME1.4/RPO time attack/Assets/Scripts/Trap_player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_player : MonoBehaviour {
+
+    public Animator animator;
+
+    private PlayerMovement playerScript;
+    public Vector2 startPosition = Vector2.zero;
+    public bool shootarrow = false;
+    public bool bowreload = false;
+    public bool shootarrow2 = false;
+    public bool bowreload2 = false;
+
+    private bool spikesprepared = true;
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        startPosition = player.transform.position; //dobi zacetno pozicijo Playerja
+        Debug.Log(startPosition);
+
+        playerScript = player.GetComponent<PlayerMovement>();
+        //shootarrow = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) //ce se sprozi trigger
+    {
+        if (other.CompareTag("KillTrap"))
+        {
+            Debug.Log("You got killed");
+            playerScript.transform.position = startPosition; //playerja vrze na zacetno pozicijo
+        }
+
+        if (other.CompareTag("SlowTrap") && spikesprepared == true)
+        {
+            float temp = playerScript.speed;
+            Debug.Log("You are slowed down for 4s");
+            animator.SetBool("Activated", true);
+            playerScript.speed = playerScript.speed * 0.3f; //playerja upočasni
+            spikesprepared = false;
+            StartCoroutine(Wait(temp)); //pocakaj 5s
+        }
+
+        if (other.CompareTag("BowTrigger") && shootarrow == false && bowreload == false)  //če player stopi v bowtriggerwire naredi ustreli puscico, in zacne reload loka
+        {
+            shootarrow = true;
+            bowreload = true;
+        }
+        if (other.CompareTag("BowTrigger2") && shootarrow2 == false && bowreload2 == false)  //če player stopi v bowtriggerwire naredi ustreli puscico, in zacne reload loka
+        {
+            shootarrow2 = true;
+            bowreload2 = true;
+        }
+    }
+    
+
+    IEnumerator Wait(float temp)
+    { 
+        yield return new WaitForSeconds(4.0f);
+        animator.SetBool("Activated", false);
+        playerScript.speed = temp;
+        Debug.Log("Znova si hiter");
+        spikesprepared = true;
+    }
+}
