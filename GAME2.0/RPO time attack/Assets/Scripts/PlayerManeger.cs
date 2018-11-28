962ce14f0db0e00dd623a5f8b6afdb6e127c17e3
Saso <saso.ivic@student.um.si>
2018-11-28 10:09:51 +0100

    Shop, kupovanje orozja/characterjev, trajno shranjevanje denarja, shrani in pristej metke kupjene v trgovini, gumb za meni v igri, gumb za trgovino v meniju.

---
+++ GAME2.0/RPO time attack/Assets/Scripts/PlayerManeger.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManeger : MonoBehaviour {
+
+    public int currentWeapon = 0;
+    public Transform[] weapons; //array za orožja
+    public int numWeapons;
+
+    int numTargets;
+
+    int numCoinStart;
+    int numCoin;
+    public int colectedCoin;
+    public int totalCoinSum;
+
+    private void Start()
+    {
+        totalCoinSum = PlayerPrefs.GetInt("money");
+        numCoinStart = GameObject.FindGameObjectsWithTag("Coin").Length;
+        numWeapons = weapons.Length;
+
+        SwitchWeapon(currentWeapon);
+    }
+
+    void Update () {
+
+        numTargets = GameObject.FindGameObjectsWithTag("Tarca").Length;
+        numCoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+        colectedCoin = numCoinStart - numCoin;
+
+        if (numTargets == 0) //vse tarce unicene
+        {
+            Debug.Log("Unicil si vse tarce vrata so se odprla");
+        }
+    }
+
+    public void ClickChangeGunButton() //Z klikom na gumb menjas orozje
+    {
+        currentWeapon++;
+        if (currentWeapon==numWeapons) // ce je zadnje orozje gre ob naslednjem kliku na prvega(krozna vrsta)
+        {
+            currentWeapon = 0;
+        }
+        SwitchWeapon(currentWeapon);
+    }
+
+    void SwitchWeapon(int index)
+    {
+        
+        for (int i=0; i<numWeapons; i++)
+        {
+            if (index != 0 && i == index && weapons[index].GetComponent<StMetkov>().numBullets != 0) //ce imas orozje pri NE-osnovnem orozju
+            {
+                Debug.Log("Metki");
+                weapons[i].gameObject.SetActive(true);
+            }
+            else if (index == 0 && i == index) //osnovno orozje ... neomejeno st metkov ... vedno lahko das na true
+            {
+                weapons[i].gameObject.SetActive(true);
+            }
+            else if(i == index && weapons[index].GetComponent<StMetkov>().numBullets == 0) //ce pri želenem orozju nimas metkov
+            {
+                Debug.Log("Nimas metkov");
+                currentWeapon = 0;
+                weapons[0].gameObject.SetActive(true); //obdrzi osnovno orozje
+            }
+            if(index!=i) //ostale daj na false
+            { 
+                weapons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void ExitGame()
+    {
+        totalCoinSum = totalCoinSum + colectedCoin; //sesteje kovance ki smo jih imeli z novimi pobranimi
+        PlayerPrefs.SetInt("money", totalCoinSum); //sharni money
+        for(int i=0; i<numWeapons; i++)
+        {
+            PlayerPrefs.SetInt("bulets"+weapons[i].name, 0); //na koncu igre so vsi metki 0
+        }
+        SceneManager.LoadScene("Menu"); //gre v meni
+    }
+
+}
---
+++ GAME2.0/RPO time attack/Assets/Scripts/VrtenjeTopa.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrtenjeTopa : MonoBehaviour {
+
+    int stevecStrelov = 0;
+    int steviloStrelov_reload;
+
+    public MoveBullet bullet;
+    public Transform firePoint; //zacetna pozicija metka
+
+    public GameObject player;
+    int gun;
+
+    public GameObject gun2;
+    public GameObject gun3;
+
+    void Update () {
+
+        int metki2 = gun2.GetComponent<StMetkov>().numBullets; //dobi skripto StMetkov (za gun2)
+        int metki3 = gun3.GetComponent<StMetkov>().numBullets; //dobi skripto StMetkov (za gun3)
+
+        PlayerManeger maneger = player.GetComponent<PlayerManeger>(); //dobi class PlayerManeger
+        gun = maneger.currentWeapon; //dobi izbrano pusko
+
+        if (gun == 0)
+        {
+            steviloStrelov_reload = 10;
+        }
+        if (gun == 1)
+        {
+            steviloStrelov_reload = 5;
+        }
+
+        //object controller = GameObject.FindGameObjectsWithTag("GameController");
+
+        Rect bounds = new Rect(0, 0, Screen.width/(3.5f), Screen.height/3); // kliki za obračanje pištole in streljanja se zaznavajo le zunaj tega lika
+       
+
+        if (Input.GetMouseButtonDown(0) && ! bounds.Contains(Input.mousePosition))
+        {
+
+            var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.back); //pistola se obrne
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponentInChildren<Animator>().SetBool("Shot", true); //animacija strela
+                StartCoroutine(Wait(stevecStrelov));
+
+                stevecStrelov++;
+
+                if (stevecStrelov > steviloStrelov_reload) {
+                    GetComponentInChildren<Animator>().SetBool("Reload", true); //animacija reload gun
+                    StartCoroutine(Wait(stevecStrelov));
+                    stevecStrelov = 0;
+                }
+
+                //ustvari metek
+                if (gun == 0)
+                {
+                    if (GetComponentInChildren<Animator>().GetBool("Reload") == false && GetComponentInChildren<Animator>().GetBool("Shot") == true)
+                    {
+                        Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    }
+                }
+
+                if (gun == 1 && metki2 > 0) //ko imaš metke pri gun2 -> metki so numBulets(pobrani v igri)+bullets2(kupljeni)
+                {
+                    metki2--; //zmanjsaj stevilo metkov (gun2)
+                    gun2.GetComponent<StMetkov>().numBullets--;
+                    if (GetComponentInChildren<Animator>().GetBool("Reload") == false && GetComponentInChildren<Animator>().GetBool("Shot") == true)
+                    {
+                        Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    }
+                }
+
+                if (gun == 1 && metki2 == 0) // ko ti zmanjka metkov pri gun2
+                {
+                    Debug.Log("Pri tej puski ni vec metkov!");
+                    GetComponentInChildren<Animator>().SetBool("Shot", false); //konec animacije strela
+                    GetComponentInChildren<Animator>().SetBool("Reload", false); //konec strela
+                }
+
+                if (gun == 2 && metki3 > 0) //ko imaš metke pri gun3
+                {
+                    metki3--; //zmanjsaj stevilo metkov (gun2)
+                    gun3.GetComponent<StMetkov>().numBullets--;
+                    if (GetComponentInChildren<Animator>().GetBool("Reload") == false && GetComponentInChildren<Animator>().GetBool("Shot") == true)
+                    {
+                        Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    }
+                }
+
+                if (gun == 2 && metki3 == 0) // ko ti zmanjka metkov pri gun3
+                {
+                    Debug.Log("Pri tej puski ni vec metkov!");
+                    GetComponentInChildren<Animator>().SetBool("Shot", false); //konec animacije strela
+                    GetComponentInChildren<Animator>().SetBool("Reload", false); //konec strela
+                }
+
+            }
+        }
+	}
+
+    IEnumerator Wait(int stevec)
+    {
+        if (stevecStrelov < steviloStrelov_reload)
+        {
+            yield return new WaitForSeconds(0.1f);
+            GetComponentInChildren<Animator>().SetBool("Shot", false); //konec strela
+        }
+        else
+        {
+            GetComponentInChildren<Animator>().SetBool("Shot", false); //dokler se reloada ne more streljat
+            yield return new WaitForSeconds(0.25f);
+            GetComponentInChildren<Animator>().SetBool("Reload", false); //konec reloada
+        }
+    }
+}
