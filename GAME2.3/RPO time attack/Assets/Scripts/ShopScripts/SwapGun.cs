fe2109a42ea585241bcdd0383e1a3babf4216ee2
zan.knuples@student.um.si <zan.knuples@student.um.si>
2018-12-02 18:36:12 +0100

    Dodan je samo AUDIO MANAGER, vsi gumbi majo nastavljen "Click" sound..
    UPORABA  (audio managerja):
    
    1.) preveri ce v tvoji sceni obstaja audio manager, ce ne ga skopiraj iz scene "Menu" (nisn fix ce treba kopirat tho)
    2.) v AUDIO MANAGER shranis zvok tak da povecas size zvokov, s tem povecas polje not dodas nov zvok npr.: sdadadsa.mp3 in si ga lahko za kasneje shranis pod enostavnim imenomkot sn naprimer naredo jaz za "Click"
    3.) Po želji lahko za vsak zvok nastavimo glasnost v AUDIO MANAGERJU, tudi pitch je tam, ce bi se kdo spilal
    
    4.) klic oz predvajanje zvoka na 2 načina:
    
    4.1)zvok dodam in kličem v Unity (navigiraj na element npr button, na desni pod komponentami izberi opcijo dodaj v tem primeru Button(script), od tam mas eno okence On Click(), v njem dodad nov event ter na tega vežeš AUDIO MANAGER tak da ga "dragaš" iz levega panela na sceni, pol na desno izberes znotraj dropdowna ta novi AUDIO MANAGER in mu zberes action Play(string), pa se v zadnje okence napišeš ime tvojega zvoka, kot si si ga shrano v AUDIO MANAGER...) POZOR: ime mora biti enako nastavljenemu, drugače ne dela!
    4.2)zvok kličem v kodi (dodajanje je enako kot pri 4.1, ampak namesto draganja AUDIO MANAGERja v Unityu lahko shranjen zvok kličeš z to vrstico kode;
    
    FindObjectOfType<AudioManager>().Play("imezvokaizUnity");   //toto kodo prilepi v scripto kje hoces da ti svira
    , spet pozor da je ime enako)

---
+++ GAME2.3/RPO time attack/Assets/Scripts/ShopScripts/SwapGun.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapGun : MonoBehaviour {
+
+
+    public int currentGun = 0;
+    public Transform[] guns;
+
+    int gunNum = 1;
+
+    public Text price5;
+    public Text price10;
+
+    public int gun1_5 = 2;
+    public int gun2_5 = 5;
+    public int gun1_10 = 5;
+    public int gun2_10 = 8;
+
+    public Button buy5Btn;
+    public Button buy10Btn;
+
+    bool lock3 = false; // za gun2
+
+    private void Start()
+    {
+        gunNum = guns.Length;
+
+        price5.text = gun1_5.ToString()+" €";
+        price10.text = gun1_10.ToString() + " €";
+
+        if (PlayerPrefs.GetInt("najboljsiCas")>20)
+        {
+            lock3 = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (currentGun==0)
+        {
+
+            if (PlayerPrefs.GetInt("money") < gun1_5) //ni dovolj denarja -> cena z rdeco barvo
+            {
+                price5.text = gun1_5.ToString() + " €";
+                price5.color = Color.red;
+            }
+            else
+            {
+                price5.text = gun1_5.ToString() + " €";
+                price5.color = new Color32(17, 245, 29, 255);
+            }
+
+            if (PlayerPrefs.GetInt("money") < gun1_10) //ni dovolj denarja -> cena z rdeco barvo
+            {
+                price10.text = gun1_10.ToString() + " €";
+                price10.color = Color.red;
+            }
+            else
+            {
+                price10.text = gun1_10.ToString() + " €";
+                price10.color = new Color32(17, 245, 29, 255);
+            }
+
+            this.GetComponent<Image>().color = new Color32(171, 192, 192, 70);
+            buy5Btn.interactable = true;
+            buy10Btn.interactable = true;
+        }
+        else if (currentGun==1)
+        {
+            if (PlayerPrefs.GetInt("money") < gun2_5) //ni dovolj denarja -> cena z rdeco barvo
+            {
+                price5.text = gun2_5.ToString() + " €";
+                price5.color = Color.red;
+            }
+            else
+            {
+                price5.text = gun2_5.ToString() + " €";
+                price5.color = new Color32(17, 245, 29, 255);
+            }
+
+            if (PlayerPrefs.GetInt("money") < gun2_10) //ni dovolj denarja -> cena z rdeco barvo
+            {
+                price10.text = gun2_10.ToString() + " €";
+                price10.color = Color.red;
+            }
+            else
+            {
+                price10.text = gun2_10.ToString() + " €";
+                price10.color = new Color32(17, 245, 29, 255);
+            }
+
+            if (lock3 == true) // zaklenjeno
+            {
+                this.GetComponent<Image>().color = new Color32(176, 32, 38, 137); //ce je zaklenjen
+                buy5Btn.interactable = false; //dissable btn
+                buy10Btn.interactable = false; //dissable btn
+            }
+            else
+            {
+                this.GetComponent<Image>().color = new Color32(171, 192, 192, 70);
+                buy5Btn.interactable = true; 
+                buy10Btn.interactable = true;
+            }
+        }
+    }
+
+    public void SwapCharLeft() //povezano z gumbom CharLeftBtn
+    {
+        if (currentGun == 0)
+        {
+            currentGun = gunNum - 1; //gre na zadnjega
+        }
+        else
+        {
+            currentGun = currentGun - 1; //eno levo
+        }
+
+        SwitchCharImg(currentGun);
+    }
+    public void SwapCharRight() //povezano z gumbom CharRightBtn
+    {
+        if (currentGun == gunNum - 1)
+        {
+            currentGun = 0; //gre na prvega
+        }
+        else
+        {
+            currentGun = currentGun + 1; //eno desno
+        }
+
+        SwitchCharImg(currentGun);
+    }
+
+    void SwitchCharImg(int index)
+    {
+        for (int i = 0; i < gunNum; i++)
+        {
+            if (i == index)
+            {
+                guns[i].gameObject.SetActive(true); //pokazi characterja
+            }
+            else
+            {
+                guns[i].gameObject.SetActive(false); //skrij characterja
+            }
+        }
+    }
+}
---
+++ GAME2.3/RPO time attack/Assets/Scripts/SpawnKeyScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKeyScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    private bool spawnkeyinfo;
+    public GameObject keyy;
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        spawnkeyinfo = player.GetComponent<PlayerManeger>().spawnkey;
+        keyy.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnkeyinfo = player.GetComponent<PlayerManeger>().spawnkey;
+        if (spawnkeyinfo == true)
+        {
+            keyy.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) //ce se sprozi trigger
+    {
+        if (other.CompareTag("Player") && spawnkeyinfo == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
