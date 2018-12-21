5b9abe0f7530accf8e21c6709e54dbb0b64217e2
Saso <saso.ivic@student.um.si>
2018-12-21 13:14:41 +0100

    Enemy health

---
+++ GAME2.9/RPO time attack/Assets/Scripts/SpriteAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAgent : MonoBehaviour
+{
+
+    public Transform target;
+
+    public GameObject player;
+
+    private void LateUpdate()
+    {
+        transform.localPosition = new Vector3(target.localPosition.x + 8.168f, target.localPosition.y - 10.393f, target.localPosition.z);
+
+        Vector3 norTar = (player.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(norTar.y, norTar.x) * Mathf.Rad2Deg;
+        Quaternion rotation = new Quaternion();
+        rotation.eulerAngles = new Vector3(0, 0, angle - 270);
+        transform.rotation = rotation;
+    }
+}
